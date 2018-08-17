6c19b6d032309ebfcd5c2dc554eb6b5d05e5a7d2
chalodk <40323079+chalodk@users.noreply.github.com>
2018-08-17 01:27:08 -0300

    Update serializers.py

--- medopricing/pricing/serializers.py
+++ medopricing/pricing/serializers.py
@@ -1,32 +1,8 @@
 from rest_framework import serializers
-from .models import Part
+from .models import Prices
+import ModelSerializer
 
-
-class PartSerializer(serializers.Serializer):
-
-	pk = serializers.IntegerField(read_only=True)
-	thickness = serializers.FloatField()
-	area = serializers.FloatField()
-	perimeter = serializers.FloatField()
-	quantity = serializers.IntegerField()
-	veneer = serializers.ChoiceField(choices=Part.VENEER_CHOICES)
-	name = serializers.ChoiceField(choices=Part.PART_CHOICES)
-	
-	def create(self, validated_data):
-		"""
-        	Create and return a new `Part` instance, given the validated data.
-		"""
-		return Part.objects.create(**validated_data)
-
-	def update(self, instance, validated_data):
-        
-        
-		instance.name = validated_data.get('name', instance.name)
-		instance.thickness = validated_data.get('thickness', instance.thickness)
-		instance.area = validated_data.get('area', instance.area)
-		instance.perimeter = validated_data.get('perimeter', instance.perimeter)
-		instance.quantity = validated_data.get('quantity', instance.quantity)
-		instance.veneer = validated_data.get('veneer', instance.veneer)
-
-		instance.save()
-		return instance
+class PricesSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Prices
+		fields = ('id','alpi1','alpi2','alpi3','alpi4','alpi5','alpi6','alpi7','alpi8','alpi9','alpi10','alpi11','contrach','prmt','qmm','dmm','tmm','T1','T2','T3','T4','T5','T6','P1','P2','P3','P4','P5')
