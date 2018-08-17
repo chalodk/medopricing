a028186e4a75e3b5b97851484badd67557b8cc47
chalodk <40323079+chalodk@users.noreply.github.com>
2018-08-17 01:17:02 -0300

    Update models.py

--- medopricing/pricing/models.py
+++ medopricing/pricing/models.py
@@ -5,56 +5,36 @@ from django.db import models
 
 # Create your models here.
 
-class Part(models.Model):
+class Prices(models.Model):
+	alpi1 = models.FloatField(blank=True)
+	alpi2 = models.FloatField(blank=True)
+	alpi3 = models.FloatField(blank=True)
+	alpi4 = models.FloatField(blank=True)
+	alpi5 = models.FloatField(blank=True)
+	alpi6 = models.FloatField(blank=True)
+	alpi7 = models.FloatField(blank=True)
+	alpi8 = models.FloatField(blank=True)
+	alpi9= models.FloatField(blank=True)
+	alpi10 = models.FloatField(blank=True)
+	alpi11 = models.FloatField(blank=True)
+	contrach = models.FloatField(blank=True)
+	prmt = models.FloatField(blank=True)
+	qmm = models.FloatField(blank=True)
+	dmm = models.FloatField(blank=True)
+	tmm = models.FloatField(blank=True)
+	T1 = models.FloatField(blank=True)
+	T2 = models.FloatField(blank=True)
+	T3 = models.FloatField(blank=True)
+	T4 = models.FloatField(blank=True)
+	T5 = models.FloatField(blank=True)
+	T6 = models.FloatField(blank=True)
+	P1 = models.FloatField(blank=True)
+	P2 = models.FloatField(blank=True)
+	P3 = models.FloatField(blank=True)
+	P4 = models.FloatField(blank=True)
+	P5 = models.FloatField(blank=True)
 
-	TABLERO = 'tablero'
-	UNION = 'union'
-	APOYOS = 'apoyos'
-
-	PART_CHOICES = (
-		(TABLERO, 'Tablero'),
-		(UNION, 'Union'),
-		(APOYOS, 'Apoyos'),
-	)
-
-
-	ALPI1 = 'alpi1'
-	ALPI2 =	'alpi2'
-	ALPI3 =	'alpi3'
-	ALPI4 =	'alpi4'
-	ALPI5 =	'alpi5'
-	ALPI6 =	'alpi6'
-	ALPI7 =	'alpi7'
-	ALPI8 =	'alpi8'
-	ALPI9 =	'alpi9'
-	ALPI10 = 'alpi10'
-	ALPI11	= 'alpi11'
-	CONTRACHAPA = 'contrach'
-
-
-	VENEER_CHOICES = (
-		(ALPI1, 'alpi1'),
-		(ALPI2,	'alpi2'),
-		(ALPI3,	'alpi3'),
-		(ALPI4,	'alpi4'),
-		(ALPI5,	'alpi5'),
-		(ALPI6,	'alpi6'),
-		(ALPI7,	'alpi7'),
-		(ALPI8,	'alpi8'),
-		(ALPI9,	'alpi9'),
-		(ALPI10, 'alpi10'),
-		(ALPI11, 'alpi11'),
-		(CONTRACHAPA, 'contrach'),
-	)
-
-
-	name = models.CharField(max_length=10, choices = PART_CHOICES)
-	thickness = models.FloatField(default=0)
-	area = models.FloatField(default=0)
-	perimeter = models.FloatField(default=0)
-	quantity = models.IntegerField(default=1)
-	veneer = models.CharField(max_length=15, choices = VENEER_CHOICES)
-
+	
 
 #class Design(models.Model):
 
