80c6da1756b40844e1cf0e886399084017f74db8
chalodk <40323079+chalodk@users.noreply.github.com>
2018-08-17 01:13:14 -0300

    Update views.py

--- medopricing/pricing/views.py
+++ medopricing/pricing/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from pricing.models import Part
+from pricing.models import Prices
 from pricing.serializers import PartSerializer
 
 
@@ -24,23 +24,23 @@ class JSONResponse(HttpResponse):
 
 @csrf_exempt
 def price(request):
-   # """
-   # List all parts, or create a new instance.
-   # """
-    #if request.method == 'GET':
-    #    series = Serie.objects.all()
-     #   serializer = SerieSerializer(series, many=True)
-      #  return JSONResponse(serializer.data)
+	#"""
+	#List all parts, or create a new instance.
+	#"""
+	if request.method == 'GET':
+		parts = Prices.objects.all()
+		serializer = SerieSerializer(series, many=True)
+		return JSONResponse(serializer.data)
 
-	if request.method == 'POST':
-		data=list() 
-		serializer = list()
-		aux = JSONParser().parse(request)
-		for i in range(len(aux)):
+	#if request.method == 'POST':
+	#	data=list() 
+	#	serializer = list()
+	#	aux = JSONParser().parse(request)
+	#	for i in range(len(aux)):
 
 			#data[i] = aux[i]  #JSONParser().parse(request[i])
-        		serializer[i] = PartSerializer(data=aux[i])
-        		if serializer[i].is_valid():
-            			serializer[i].save()
-            			return JSONResponse(serializer.data, status=201)
-        		return JSONResponse(serializer.errors, status=400)
+        #		serializer[i] = PartSerializer(data=aux[i])
+        #		if serializer[i].is_valid():
+         #   			serializer[i].save()
+          #  			return JSONResponse(serializer.data, status=201)
+        #		return JSONResponse(serializer.errors, status=400)
