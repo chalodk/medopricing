db4b79773a82bf1175981a6a4790d5caecbb070e
chalodk <40323079+chalodk@users.noreply.github.com>
2018-08-22 13:12:06 -0300

    Update admin.py

--- medopricing/pricing/admin.py
+++ medopricing/pricing/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class FeedItemAdmin(admin.ModelAdmin):
+  list_display = ('alpi1','alpi2','contrach')
+  admin.site.register(models.Prices, PricesAdmin)
