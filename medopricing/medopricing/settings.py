94795aa250d02426a38bb57ad4df8a442242daf7
Gonzalo Arellano <chalo.dre@gmail.com>
2018-08-24 14:55:11 +0000

    hello

--- medopricing/medopricing/settings.py
+++ medopricing/medopricing/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS=['api.me-do.cl']
+ALLOWED_HOSTS=['api.me-do.cl', '127.0.0.1']
--- medopricing/pricing/admin.py
+++ medopricing/pricing/admin.py
@@ -1,9 +1,11 @@
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-#from . import models
+from pricing.models import Prices
 
 # Register your models here.
-#class FeedItemAdmin(admin.ModelAdmin):
- # list_display = ('alpi1','alpi2','contrach')
-  #admin.site.register(models.Prices, PricesAdmin)
+class PricingAdmin(admin.ModelAdmin):
+	list_display = ('alpi1','alpi2','contrach')
+
+admin.site.register(Prices, PricingAdmin)
